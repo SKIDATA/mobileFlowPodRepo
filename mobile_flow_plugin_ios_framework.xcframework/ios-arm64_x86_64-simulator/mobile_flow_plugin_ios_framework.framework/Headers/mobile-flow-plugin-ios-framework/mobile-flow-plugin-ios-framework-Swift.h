2095c8aa2c7db08268adf65f2b3f715899dde783
Denis Schüle <denis.schuele@skidata.com>
2022-01-11 11:18:58 +0100

    update to Version 1.5.6122-SNAPSHOT

--- mobile_flow_plugin_ios_framework.xcframework/ios-arm64_x86_64-simulator/mobile_flow_plugin_ios_framework.framework/Headers/mobile-flow-plugin-ios-framework/mobile-flow-plugin-ios-framework-Swift.h
+++ mobile_flow_plugin_ios_framework.xcframework/ios-arm64_x86_64-simulator/mobile_flow_plugin_ios_framework.framework/Headers/mobile-flow-plugin-ios-framework/mobile-flow-plugin-ios-framework-Swift.h
@@ -297,11 +297,6 @@ SWIFT_PROTOCOL("_TtP32mobile_flow_plugin_ios_framework24MobileFlowPluginDelegate
 /// The company id of the left region.
 ///
 - (void)leftRegion:(NSString * _Nonnull)companyId;
-/// This method is called if a gate is accessed.
-/// \param gateId 
-/// The gate id of the accessed gate.
-///
-- (void)accessedGate:(NSString * _Nonnull)gateId;
 @end
 
 
@@ -647,11 +642,6 @@ SWIFT_PROTOCOL("_TtP32mobile_flow_plugin_ios_framework24MobileFlowPluginDelegate
 /// The company id of the left region.
 ///
 - (void)leftRegion:(NSString * _Nonnull)companyId;
-/// This method is called if a gate is accessed.
-/// \param gateId 
-/// The gate id of the accessed gate.
-///
-- (void)accessedGate:(NSString * _Nonnull)gateId;
 @end
 
 
