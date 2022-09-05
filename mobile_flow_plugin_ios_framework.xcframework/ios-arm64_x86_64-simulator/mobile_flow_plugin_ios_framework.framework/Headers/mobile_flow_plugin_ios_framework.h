5cab59e1f87faf0ecff0d6e3329fc7f3cad01768
Denis Schüle <denis.schuele@skidata.com>
2022-09-05 13:45:31 +0200

    update to Version 1.6.90018-SNAPSHOT

---
+++ mobile_flow_plugin_ios_framework.xcframework/ios-arm64_x86_64-simulator/mobile_flow_plugin_ios_framework.framework/Headers/mobile_flow_plugin_ios_framework.h
@@ -0,0 +1,19 @@
+
+
+//
+//  mobile_flow_plugin_ios_framework.h
+//  mobile-flow-plugin-ios-framework
+//
+//  Created by inno on 25.01.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mobile_flow_plugin_ios_framework.
+FOUNDATION_EXPORT double mobile_flow_plugin_ios_frameworkVersionNumber;
+
+//! Project version string for mobile_flow_plugin_ios_framework.
+FOUNDATION_EXPORT const unsigned char mobile_flow_plugin_ios_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mobile_flow_plugin_ios_framework/PublicHeader.h>
+
